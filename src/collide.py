7bc26b81d5be77cbfcbffeb52c760ea0c4623296
olgahuusari <olga.huusari@helsinki.fi>
2024-04-23 19:27:10 +0300

    modified

--- src/collide.py
+++ src/collide.py
@@ -8,7 +8,6 @@ class Collide:
         if (asteroid.x-laser.img.get_width() < laser.x < asteroid.x+asteroid.img.get_width()) and (
         asteroid.y-laser.img.get_height() < laser.y < asteroid.y+asteroid.img.get_height()):
             self.laser_asteroid = True
-            print(laser.x, laser.y, laser.img.get_width(), laser.img.get_height(), asteroid.x, asteroid.y, asteroid.img.get_width(), asteroid.img.get_height())
 
     def asteroid_hit_ship(self, asteroid, ship):
         if (ship.x-asteroid.img.get_width() < asteroid.x < ship.x) and (
